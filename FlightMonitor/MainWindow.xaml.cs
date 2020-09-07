efab8093954b9c46e8068297f0aae43931f11e95
Andrew-William-Smith <aws@awsmith.us>
2020-09-07 12:17:55 -0500

    Basic SimConnect interop working
    Simple interop with SimConnect (FS2020 API) is now operational: the application can connect to a running instance of FS2020 and display popup messages.

---
+++ FlightMonitor/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace FlightMonitor {
+    public partial class MainWindow : Window, INotifyPropertyChanged {
+        /// <summary>Client used to access simulator data.</summary>
+        private readonly SimConnectClient simClient;
+
+        // Window bindings
+        public string WindowTitleStatus => "Flight Monitor — " + (simClient.Connected ? "Connected" : "Disconnected");
+
+        // Connect button bindings
+        public string ConnectButtonText => (simClient.Connected ? "Disconnect from" : "Connect to") + " Flight Simulator";
+        public bool ConnectButtonChecked {
+            get => simClient.Connected;
+            set {
+                if (simClient.Connected) {
+                    simClient.Disconnect();
+                } else {
+                    _ = simClient.Connect();
+                }
+                NotifyPropertyChanged("ConnectButtonText");
+                NotifyPropertyChanged("WindowTitleStatus");
+            }
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+            simClient = new SimConnectClient();
+
+            // Connect WPF bindings once data sources are initialised
+            DataContext = this;
+        }
+
+        /// <summary>Get the <c>HwndSource</c> for this window.</summary>
+        private HwndSource GetHwndSource() => PresentationSource.FromVisual(this) as HwndSource;
+
+        protected override void OnSourceInitialized(EventArgs e) {
+            base.OnSourceInitialized(e);
+            // Set up Win32 message loop interop routines
+            GetHwndSource().AddHook(WndProc);
+            simClient.WindowHandle = GetHwndSource().Handle;
+        }
+
+        /// <summary>Win32 message loop interop.</summary>
+        private IntPtr WndProc(IntPtr hWnd, int iMsg, IntPtr hWParam, IntPtr hLParam, ref bool bHandled) {
+            try {
+                if (iMsg == SimConnectClient.WM_USER_SIMCONNECT) {
+                    // Whenever a SimConnect event occurs, allow the client to process the message
+                    simClient.ReceiveMessage();
+                }
+            } catch {
+                simClient.Disconnect();
+            }
+            return IntPtr.Zero;
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ FlightMonitor/SimConnectClient.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.FlightSimulator.SimConnect;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace FlightMonitor {
+    /// <summary>
+    /// A client for the Flight Simulator SimConnect API, provided by default with the Flight Simulator 2020 SDK.
+    /// Handles all communication with a running instance of the game, providing data to the server and application.
+    /// </summary>
+    class SimConnectClient {
+        /// <summary>
+        /// A placeholder for SimConnect enum types that don't accept integers.
+        /// </summary>
+        private enum DummyEnum {
+            Dummy
+        }
+
+        /// <summary>Win32 event for SimConnect tasks.</summary>
+        public const uint WM_USER_SIMCONNECT = 0x0402;
+
+        /// <summary>The duration in seconds for which text messages should be shown in the simulator.</summary>
+        private const float TEXT_DURATION = 3.0f;
+
+        /// <summary>The window handle on whose event loop SimConnect is running.</summary>
+        public IntPtr WindowHandle { get; set; } = IntPtr.Zero;
+
+        /// <summary>SimConnect handle for interacting with an instance of the simulator.</summary>
+        private SimConnect connection;
+
+        /// <summary>Whether this client is currently connected to the simulator.</summary>
+        public bool Connected => connection != null;
+
+        private void SimConnect_RecvOpenHandler(SimConnect sender, SIMCONNECT_RECV_OPEN data) {
+            // Show a message in the simulator to show that the connection succeeded
+            sender.Text(SIMCONNECT_TEXT_TYPE.PRINT_WHITE, TEXT_DURATION, DummyEnum.Dummy,
+                "Flight Monitor is connected to this simulator instance.");
+        }
+
+        /// <summary>
+        /// Connect this instance to the active simulator's SimConnect handle.
+        /// </summary>
+        /// <returns>Whether the connection was established successfully.</returns>
+        public bool Connect() {
+            if (connection != null) {
+                // If we already have a connection, there is no need to re-establish one
+                return true;
+            }
+
+            try {
+                // Establish a connection to the simulator on the current window's event loop
+                connection = new SimConnect("Flight Monitor", WindowHandle, WM_USER_SIMCONNECT, null,
+                    SimConnect.SIMCONNECT_OPEN_CONFIGINDEX_LOCAL);
+
+                // Add connection event handlers
+                connection.OnRecvOpen += new SimConnect.RecvOpenEventHandler(SimConnect_RecvOpenHandler);
+                return true;
+            } catch (COMException ex) {
+                _ = MessageBox.Show($"An error occurred while connecting to Flight Simulator:\r\n{ex.Message}",
+                    "Connection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Disconnect this instance from the active simulator's SimConnect handle.
+        /// </summary>
+        public void Disconnect() {
+            connection.Text(SIMCONNECT_TEXT_TYPE.PRINT_WHITE, TEXT_DURATION, DummyEnum.Dummy,
+                "Flight Monitor disconnected!");
+            connection.Dispose();
+            connection = null;
+        }
+
+        /// <summary>
+        /// Receive a message from the SimConnect API.  Should only be called from the Win32 message loop.
+        /// </summary>
+        public void ReceiveMessage() {
+            connection?.ReceiveMessage();
+        }
+    }
+}
