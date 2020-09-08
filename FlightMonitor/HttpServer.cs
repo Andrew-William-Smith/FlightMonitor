3dbbd2c8d666250a6a0d0fb6a3830dd9db03e67e
Andrew-William-Smith <aws@awsmith.us>
2020-09-08 14:21:21 -0500

    Added React client; handle HTTP requests in separate threads

--- FlightMonitor/HttpServer.cs
+++ FlightMonitor/HttpServer.cs
@@ -1,9 +1,9 @@
 ﻿using System.IO;
 using System.Net;
-using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace FlightMonitor {
     /// <summary>
@@ -30,40 +30,61 @@ namespace FlightMonitor {
         }
 
         /// <summary>
-        /// Handle incoming HTTP connections.  Intended to be run in a separate thread cancellable with the specified
-        /// token.
+        /// Handle an incoming HTTP connection with the specified context.  Intended to be run in a separate thread off
+        /// of the main server thread.
+        /// </summary>
+        /// <param name="context">The HTTP context of the connection.</param>
+        private void HandleHttpConnection(HttpListenerContext context) {
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            // Attempt to get the contents of the specified file
+            string requestedFile = request.RawUrl == "/" ? "/index.html" : request.RawUrl;
+            byte[] fileContents;
+            try {
+                fileContents = File.ReadAllBytes($"./Client{requestedFile}");
+                response.ContentType = MimeMapping.GetMimeMapping(requestedFile);
+            } catch {
+                fileContents = Encoding.UTF8.GetBytes($"Error 404: File {requestedFile} could not be found.");
+                response.ContentType = "text/plain";
+                response.StatusCode = 404;
+            }
+
+            // Send response back to the client
+            response.ContentEncoding = Encoding.UTF8;
+            response.ContentLength64 = fileContents.LongLength;
+            try {
+                response.OutputStream.Write(fileContents, 0, fileContents.Length);
+            } catch (HttpListenerException) {
+                /* Occasionally, writing to the response's output stream will result in an HttpListenerException
+                 * because the browser has closed the channel before HttpListener was expecting it to.  This is
+                 * well-documented, harmless, and can be safely ignored. */
+            }
+            response.Close();
+        }
+
+        /// <summary>
+        /// Handle incoming HTTP and WebSocket connections.  Intended to be run in a separate thread cancellable with
+        /// the specified token.
         /// </summary>
         /// <param name="cancelToken">The cancellation token used to stop the thread.</param>
         private void HandleConnections(CancellationToken cancelToken) {
             // Run until the cancellation token signals to stop
             while (!cancelToken.IsCancellationRequested) {
-                // Get the request and response from the connection context
-                HttpListenerContext context;
+                // For HTTP requests, spawn a new HTTP thread to handle the request
                 try {
-                    context = listener.GetContext();
+                    HttpListenerContext context = listener.GetContext();
+                    _ = Task.Run(() => {
+                        try {
+                            HandleHttpConnection(context);
+                        } catch (HttpListenerException) {
+                            /* This exception is almost definitely a result of the server being terminated, so there's no
+                             * harm in ignoring it since we're going down anyway. */
+                        }
+                    }, cancelToken);
                 } catch (HttpListenerException) {
-                    return;
+                    // Safe to ignore
                 }
-                HttpListenerRequest request = context.Request;
-                HttpListenerResponse response = context.Response;
-
-                // Attempt to get the contents of the specified file
-                string requestedFile = request.RawUrl == "/" ? "/index.html" : request.RawUrl;
-                byte[] fileContents;
-                try {
-                    fileContents = File.ReadAllBytes($"./Client{requestedFile}");
-                    response.ContentType = "text/html";
-                } catch {
-                    fileContents = Encoding.UTF8.GetBytes($"Error 404: File {requestedFile} could not be found.");
-                    response.ContentType = "text/plain";
-                    response.StatusCode = 404;
-                }
-
-                // Send response back to the client
-                response.ContentEncoding = Encoding.UTF8;
-                response.ContentLength64 = fileContents.LongLength;
-                response.OutputStream.Write(fileContents, 0, fileContents.Length);
-                response.Close();
             }
         }
 
@@ -75,7 +96,7 @@ namespace FlightMonitor {
 
             // Start a new thread to handle requests
             cancelSource = new CancellationTokenSource();
-            _ = Task.Factory.StartNew(() => HandleConnections(cancelSource.Token), cancelSource.Token);
+            _ = Task.Run(() => HandleConnections(cancelSource.Token), cancelSource.Token);
         }
 
         /// <summary>
